092aaf6de89b15c906caef1581f386dd325c24bd
Ilya Kazakov <robotbotyourube@gmail.com>
2024-12-23 19:37:36 +0300

    Add settings to navigation and move logic of navigation in NavigationState

---
+++ app/src/main/java/com/robotbot/vknewsclient/navigation/NavigationState.kt
@@ -0,0 +1,30 @@
+package com.robotbot.vknewsclient.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+class NavigationState(
+    val navHostController: NavHostController
+) {
+
+    fun navigateTo(route: String) {
+        navHostController.navigate(route) {
+            launchSingleTop = true
+            popUpTo(Screen.NewsFeed.route) {
+                saveState = true
+            }
+            restoreState = true
+        }
+    }
+}
+
+@Composable
+fun rememberNavigationState(
+    navHostController: NavHostController = rememberNavController()
+) : NavigationState {
+    return remember {
+        NavigationState(navHostController)
+    }
+}
--- app/src/main/java/com/robotbot/vknewsclient/ui/theme/VkNewsMainScreen.kt
+++ app/src/main/java/com/robotbot/vknewsclient/ui/theme/VkNewsMainScreen.kt
@@ -1,16 +1,8 @@
 package com.robotbot.vknewsclient.ui.theme
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -18,39 +10,31 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SwipeToDismissBox
-import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
 import com.robotbot.vknewsclient.MainViewModel
 import com.robotbot.vknewsclient.navigation.AppNavGraph
+import com.robotbot.vknewsclient.navigation.rememberNavigationState
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
 
-    val navHostController = rememberNavController()
+    val navigationState = rememberNavigationState()
 
     Scaffold(
         bottomBar = {
             NavigationBar(
                 containerColor = MaterialTheme.colorScheme.primary
             ) {
-                val navBackStackEntry by navHostController.currentBackStackEntryAsState()
+                val navBackStackEntry by navigationState.navHostController.currentBackStackEntryAsState()
                 val currentRoute = navBackStackEntry?.destination?.route
 
                 val items = listOf(
@@ -61,7 +45,7 @@ fun MainScreen(viewModel: MainViewModel) {
                 items.forEach { item ->
                     NavigationBarItem(
                         selected = currentRoute == item.screen.route,
-                        onClick = { navHostController.navigate(item.screen.route) },
+                        onClick = { navigationState.navigateTo(item.screen.route) },
                         icon = {
                             Icon(item.icon, contentDescription = null)
                         },
@@ -81,7 +65,7 @@ fun MainScreen(viewModel: MainViewModel) {
         }
     ) { paddingValues ->
         AppNavGraph(
-            navHostController = navHostController,
+            navHostController = navigationState.navHostController,
             homeScreenContent = {
                 HomeScreen(viewModel = viewModel, paddingValues = paddingValues)
             },
@@ -103,7 +87,7 @@ fun MainScreen(viewModel: MainViewModel) {
 
 @Composable
 private fun TextCounter(name: String, modifier: Modifier = Modifier) {
-    var counter by remember {
+    var counter by rememberSaveable {
         mutableStateOf(0)
     }
 
